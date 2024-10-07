a37b22a182072b94220a9fb010c115b1cad9dd05
Al Stone <astone@heckgrammar.co.uk>
2024-10-07 09:57:37 +0100

    concat

--- StringHandling12D24/Program.cs
+++ StringHandling12D24/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StringHandling12D24
+﻿using System.Data.SqlTypes;
+
+namespace StringHandling12D24
 {
     internal class Program
     {
@@ -6,7 +8,28 @@
         {
             string aString = "String handling";
             string bString = "operations";
+            //• length
+            Console.WriteLine(aString.Length);
+            //• position
+            Console.WriteLine(aString[0]);
+            Console.WriteLine(aString[aString.Length - 1]);
+            //   Console.WriteLine(aString[aString.Length]); outside bounds of the array
 
+            //• substring
+            Console.WriteLine(aString.Substring(3,5));//starts index 3 and has a length of 5 characters
+            Console.WriteLine(aString.Substring(10)); // starts at index 10 and goes to the end
+            // how would you output the last 3 characters of any string
+            Console.WriteLine(aString.Substring(aString.Length-3));
+            Console.WriteLine(bString.Substring(bString.Length-3));
+
+            //• concatenation
+            Console.WriteLine(aString + " " + bString);
+            // String builders
+            Console.WriteLine($"this is the first string {aString} and this is the second string {bString}");
+            //• character → character code
+
+            //• character code → character
+            //• string conversion operations
         }
     }
 }
